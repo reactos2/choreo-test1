fae8fe42c8d3525754d641843d80100ad00d2c86
reactos2 <164987684+reactos2@users.noreply.github.com>
2024-03-26 06:07:20 +0000

    test gin 1

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello, test")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	r := gin.Default()
+
+	r.GET("/", handleHomePage)
+
+	r.GET("/:name", func(c *gin.Context) {
+		name := c.Param("name")
+
+		c.JSON(200, gin.H{
+			"message": fmt.Sprintf("Hello, %s!", name),
+		})
+	})
+
+	r.Run(fmt.Sprintf(":%s", port))
+}
+
+func handleHomePage(ctx *gin.Context) {
+	ctx.String(200, "welcome to koyeb test 1")
 }
